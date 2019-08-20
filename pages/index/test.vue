647c3ff30fdbe9313e2389c4db272a7ff7071977
wangdanfeng <1204738681@qq.com>
2019-08-20 18:19:00 +0800

    12

---
+++ pages/index/test.vue
@@ -0,0 +1,18 @@
+<template>
+	<view>
+		<text @click="handClick">子组建</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		methods: {
+			handClick() {
+				console.log('子组件')
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
